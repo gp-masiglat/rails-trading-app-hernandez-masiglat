7ad5fe7f7ca6ebbe86c65e160e629816cc8981d5
JennieLin08 <jennelyn.hernandez2578@gmail.com>
2024-01-04 20:39:30 +0800

    add balance

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_15_044428) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_04_121739) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_15_044428) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "balance"
   end
 
 end
