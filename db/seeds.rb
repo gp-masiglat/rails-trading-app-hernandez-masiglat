ba567d64ca607152a9868633faf4ce8e871d5f71
Gerome <gerome.pm@gmail.com>
2024-01-12 04:37:18 +0800

    Add admin creation to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,4 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create([{username: "admin", email_address: "admin@admin.com", password: "password", password_confirmation: "password", full_name: "Admin Account", role: "Admin", status: "Approved" }])
